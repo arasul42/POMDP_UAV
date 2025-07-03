8889775c1fe77a20daff1c3cf05d223154eb3710
arasul42 <ashik.rasul@outlook.com>
2025-07-03 11:54:02 -0500

    belief  updata

---
+++ grid_world.jl
@@ -0,0 +1,13 @@
+# using Pkg
+
+# Pkg.add("POMDPs")
+# Pkg.add("POMDPTools")
+# Pkg.add("DiscreteValueIteration")
+# Pkg.add("MCTS")
+
+# Pkg.update()
+
+using POMDPs
+using POMDPTools
+using DiscreteValueIteration
+using MCTS
